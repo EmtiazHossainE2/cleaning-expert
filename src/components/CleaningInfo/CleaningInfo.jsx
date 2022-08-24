c22c102e2600ee1693319785305579efaa6e3762
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-24 19:08:14 +0600

    cleaning info added

--- src/components/CleaningInfo/CleaningInfo.jsx
+++ src/components/CleaningInfo/CleaningInfo.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import { houseCleaningService } from '../../data';
+import { Benefits, Equipment, houseCleaningService, SafetyPrecautions } from '../../data';
 
 const CleaningInfo = () => {
   return (
     <div className='container mx-auto my-12 lg:pt-12'>
-      <div className="border px-5 lg:mx-64 pb-20" style={{ fontFamily: 'Mulish' }}>
-        <h2 className='font-bold text-xl pt-8 pb-10' >Top 10 House Cleaning Service in Delhi NCR :</h2>
-        <h4 className='font-bold text-lg'>Services Included in House Cleaning Services:</h4>
+      <div className="border px-5 lg:mx-64 pb-12" style={{ fontFamily: 'Mulish' }}>
+        <h2 className='font-bold text-xl pt-8 ' >Top 10 House Cleaning Service in Delhi NCR :</h2>
+
+        {/* House Cleaning Service  */}
+        <h4 className='font-bold text-lg pt-8'>Services Included in House Cleaning Services:</h4>
         <hr className='my-4'/>
         {houseCleaningService?.map((data , index) => {
+          return <p key={data?.id}>
+            {index + 1}. {data?.text}
+          </p>
+        })}
+
+        {/* Equipment  */}
+        <h4 className='font-bold text-lg pt-16'>Materials & Equipment's Used In House Cleaning Services:</h4>
+        <hr className='my-4'/>
+        {Equipment?.map((data , index) => {
+          return <p key={data?.id}>
+            {index + 1}. {data?.text}
+          </p>
+        })}
+
+        {/* Benefits Of Booking :  */}
+        <h4 className='font-bold text-lg pt-16'>Benefits Of Booking Us For Home Cleaning Services:</h4>
+        <hr className='my-4'/>
+        {Benefits?.map((data , index) => {
+          return <p key={data?.id}>
+            {index + 1}. {data?.text}
+          </p>
+        })}
+
+        {/* Safety Precautions:  */}
+        <h4 className='font-bold text-lg pt-16'>Safety Precautions Follows In House Cleaning Services:</h4>
+        <hr className='my-4'/>
+        {SafetyPrecautions?.map((data , index) => {
           return <p key={data?.id}>
             {index + 1}. {data?.text}
           </p>
--- src/data.js
+++ src/data.js
@@ -33,7 +33,7 @@ export const houseCleaningService = [
   },
   {
     id:9,
-    text:'cleaning and Polishing '
+    text:'Cleaning and Polishing '
   },
   {
     id:10,
@@ -46,5 +46,69 @@ export const houseCleaningService = [
   {
     id:12,
     text:'Oil layer on the kitchen wall will be removed by the kitchen cleaner Water stains, pan stains, or floor stains in the bathroom will be removed'
+  },
+]
+
+export const Equipment = [
+  {
+    id:311,
+    text:'Professional cleaning chemicals.'
+  },
+  {
+    id:312,
+    text:'Single-disc floor scrubber/handheld scrubber'
+  },
+  {
+    id:313,
+    text:'Vacuum Cleaner'
+  },
+  {
+    id:314,
+    text:'Microfiber Cloths for dusting'
+  },
+  {
+    id:315,
+    text:'Mops, Scotch Brite / foam or scrubbers.'
+  },
+  {
+    id:316,
+    text:'Ladders, bucket, wiper and mugs etc.'
+  },
+]
+
+export const Benefits = [
+  {
+    id:3101,
+    text: 'Best price guarantee: We offer you the best and lowest price offered in the market.Five Star Rated Experts: Providing you with the top'
+  },
+  {
+    id:3102,
+    text: 'Certified and accomplished practitioners. Timely Service: Our specialists are committed to delivering timely services.'
+  },
+  {
+    id:3103,
+    text: '100% safe chemicals usage: Our Experts use environmentally friendly chemicals'
+  },
+]
+export const SafetyPrecautions = [
+  {
+    id:4101,
+    text: '1. Minimal touch contact & social distancing'
+  },
+  {
+    id:4102,
+    text: 'Daily temperature monitoring.'
+  },
+  {
+    id:4103,
+    text: 'Wearing masks and gloves.'
+  },
+  {
+    id:4104,
+    text: 'Regular sanitization of hands.'
+  },
+  {
+    id:4105,
+    text: 'Compliant of the Aarogaya Setu App.'
   },
 ]
