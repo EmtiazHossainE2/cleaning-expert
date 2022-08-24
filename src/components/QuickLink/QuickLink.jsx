8a1111d8d59fb4d84eb6a865bcd2160fd1804813
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-25 00:32:14 +0600

    quick link added

--- src/components/QuickLink/QuickLink.jsx
+++ src/components/QuickLink/QuickLink.jsx
@@ -4,14 +4,17 @@ const QuickLink = () => {
   return (
     <div className='container mx-auto lg:px-[230px]'>
       <h2 className='font-[500] text-3xl pt-5'>Quick Links</h2>
-      <h3 className='text-[22px] font-[500]'>Serving in</h3>
-      <div className='cursor-pointer'>
-        <p>Ludhiana • Kochi • Kanpur • Agra • Bhopal • Guwahati • Vijayawada • Varanasi • Coimbatore • Patna • Raipur </p>
-        <p>Jamshedpur • Dehradun • Meerut • Ranchi • Prayagraj • Amritsar • Gwalior • Kota • Aurangabad 
-          • Mysore Guntur </p>
-        <p>Chennai • Delhi NCR • Chandigarh Tricity • Jaipur • Hyderabad • Kolkata • Mumbai • Pune • Nagpur • Visakhapatnam • Vadodara • Lucknow • Bhubaneswar • Surat• Cuttack • Udaipur • Ahmedabad • Bangalore • </p>
-        <p>Indore • Rajahmundry • Madurai • Jodhpur • Kakinada • Warangal • Sonipat • Panipat • Karnal • Rohtak • Alwar </p>
-      </div>
+      <h3 className='text-[22px] font-[500] pt-5'>Serving in</h3>
+      <p className='cursor-pointer'>Ludhiana • Kochi • Kanpur • Agra • Bhopal • Guwahati • Vijayawada • Varanasi • Coimbatore • Thiruvananthapuram • Patna • Raipur • Nashik • Jabalpur • Jamshedpur • Dehradun • Meerut • Ranchi • Prayagraj • Amritsar • Gwalior • Kota • Aurangabad • Mysore • Guntur • Cuttack • Udaipur • Ahmedabad • Bangalore • Chennai • Delhi NCR • Chandigarh Tricity • Jaipur • Hyderabad • Kolkata • Mumbai • Pune • Nagpur • Visakhapatnam • Vadodara • Lucknow • Bhubaneswar • Surat • Indore • Rajahmundry • Madurai • Jodhpur • Kakinada • Warangal • Sonipat • Panipat • Karnal • Rohtak • Alwar </p>
+
+      <h3 className='text-[22px] font-[500] pt-5'>Also available in</h3>
+      <p className='cursor-pointer'>Full House Cleaning Service in Delhi NCR, India • Full House Cleaning Service in Kharkhari Jatmal, South Delhi, Delhi NCR, India • Full House Cleaning Service in Raja Garden, West Delhi, Delhi NCR, India • Full House Cleaning Service in Badusarai, South Delhi, Delhi NCR, India • Full House Cleaning Service in Nizamuddin West, South Delhi, Delhi NCR, India • Full House Cleaning Service in Kapashera, South Delhi, Delhi NCR, India • Full House Cleaning Service in Budh Vihar, North Delhi, Delhi NCR, India • Full House Cleaning Service in Karam Pura, West Delhi, Delhi NCR, India • Full House Cleaning Service in Yusuf Sarai, South Delhi, Delhi NCR, India • Full House Cleaning Service in Mandawali, East Delhi, Delhi NCR, India • </p>
+
+
+      <h3 className='text-[22px] font-[500] pt-5'>Services Offered</h3>
+      <p className='cursor-pointer'>Sofa Cleaning in Delhi NCR, India • Bathroom Cleaning in Delhi NCR, India • Kitchen Cleaning in Delhi NCR, India • Office Cleaning in Delhi NCR, India • Car Cleaning in Delhi NCR, India • </p>
+
+
     </div>
   );
 };
