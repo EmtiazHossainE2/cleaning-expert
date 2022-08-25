f386aae51fbe22b16cf70c17b66a0e58ccedd01a
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-26 00:48:57 +0600

    option added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,9 +6,24 @@ const Form = () => {
       <form className='space-y-4'>
         <select name="" id="" className='border border-black rounded-md w-full py-1'>
           <option value="Select Pest Control Options">Select Pest Control Options</option>
-          <option value="Select Pest Control Options">Select Pest Control Options</option>
-          <option value="Select Pest Control Options">Select Pest Control Options</option>
-          <option value="Select Pest Control Options">Select Pest Control Options</option>
+          <option value="Deep Home Cleaning - 1 BHK Vacant">Deep Home Cleaning - 1 BHK Vacant</option>
+          <option value="Deep Home Cleaning - 2 BHK Vacant">Deep Home Cleaning - 2 BHK Vacant</option>
+          <option value="Deep Home Cleaning - 3 BHK Vacant">Deep Home Cleaning - 3 BHK Vacant</option>
+          <option value="Deep Home Cleaning - 4 BHK Vacant">Deep Home Cleaning - 4 BHK Vacant</option>
+          <option value="Deep Home Cleaning - 5 BHK Vacant">Deep Home Cleaning - 5 BHK Vacant</option>
+          <option value="Deep Home Cleaning - 1 BHK Occupied">Deep Home Cleaning - 1 BHK Occupied</option>
+          <option value="Deep Home Cleaning - 2 BHK Occupied">Deep Home Cleaning - 2 BHK Occupied</option>
+          <option value="Deep Home Cleaning - 3 BHK Occupied">Deep Home Cleaning - 3 BHK Occupied</option>
+          <option value="Deep Home Cleaning - 4 BHK Occupied">Deep Home Cleaning - 4 BHK Occupied</option>
+          <option value="Deep Home Cleaning - 5 BHK Occupied">Deep Home Cleaning - 5 BHK Occupied</option>
+          <option value="Deep Home Cleaning - 1 Room Kitchen Set">Deep Home Cleaning - 1 Room Kitchen Set</option>
+          <option value="Deep Home Cleaning - 1 Room (upto 200sq ft)">Deep Home Cleaning - 1 (upto 200sq ft)</option>
+          <option value="Deep Home Cleaning - 1 Room 1 Bathroom">Deep Home Cleaning - 1 Room 1 Bathroom</option>
+          <option value="Deep Home Cleaning - 1 Room 1 Balcony">Deep Home Cleaning - 1 Room 1 Balcony</option>
+          <option value="Deep Home Cleaning Villa - 2K-3K sqft">Deep Home Cleaning  Villa - 2K-3K sqft</option>
+          <option value="Deep Home Cleaning Villa - 3K-4K sqft">Deep Home Cleaning  Villa - 3K-4K sqft</option>
+          <option value="Deep Home Cleaning Villa - 4K-5K sqft">Deep Home Cleaning  Villa - 4K-5K sqft</option>
+          <option value="Deep Home Cleaning Villa - 5K-6K sqft">Deep Home Cleaning  Villa - 5K-6K sqft</option>
         </select>
         <div className='flex flex-col md:flex-row justify-between gap-2'>
           <input className='border border-[#878686] rounded-md py-1 pl-2 ' type="text" name="name" id="name" placeholder='Enter Name' required />
