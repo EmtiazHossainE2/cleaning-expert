61392de69867511d9d3649177c4ec408ce6b1753
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-08-26 00:40:32 +0600

    banner image handle

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,12 +5,12 @@ import './Banner.css'
 const Banner = () => {
   return (
     <div className="bg-container">
-      <div className="py-16 bg-overlay">
+      <div className="py-16 ">
         <div className="container mx-auto">
           <div className="flex flex-col lg:flex-row justify-between px-5 lg:px-12 ">
             <div className='flex flex-col justify-center font-bold space-y-2 text-white'>
-              <h1 className="text-lg lg:text-2xl ">Top House Cleaning Services </h1>
-              <h1 className="text-lg lg:text-2xl lg:pb-0">In Delhi NCR</h1>
+              <h1 className="text-lg lg:text-4xl ">Top House Cleaning Services </h1>
+              <h1 className="text-lg lg:text-4xl lg:pb-0">In Delhi NCR</h1>
             </div>
             <Form/>
           </div>
